728b562b7f7cbcb27cbd7e4b9599ab29766d0211
Nick Doboszenski <84477153+Waffleer@users.noreply.github.com>
2025-09-24 10:53:47 -0500

    Implementation of Basic e2e Testing (#1)
    * Bad commit
    
    * Add pipeline test for cat
    
    * Add nightwatch template files and basic tests
    
    * Fixed readme word
    
    ---------
    
    Co-authored-by: Nicholas Doboszenski <waffleer@orangeSlicer>

---
+++ mytestvueapp.client/nightwatch/pixelpainter.home.ts
@@ -0,0 +1,63 @@
+import {assert, ExtendDescribeThis} from 'nightwatch';
+
+interface CustomThis {
+  pixelpainterurl_base: string;
+  pixelpainterurl_goto: string;
+}
+
+// callback passed to `describe` should be a regular function (not an arrow function).
+describe('Pixel Painter Tests', function(this: ExtendDescribeThis<CustomThis>) {
+  this.pixelpainterurl_base = 'https://localhost:5173/';
+  this.pixelpainterurl_goto = this.pixelpainterurl_base
+  
+  // callback can be a regular function as well as an arrow function.
+  beforeEach(function(this: ExtendDescribeThis<CustomThis>, browser) {
+    browser.navigateTo(this.pixelpainterurl_goto!);
+  });
+
+  it('Find Cat', (browser) => {
+    browser
+      .element.findByAltText('cat')
+      .assert.visible();
+  });
+
+  it('Home button', (browser) => {
+    browser
+      .element.findByText('Home')
+      .assert.visible();
+    browser
+      .element.findByText('Home')
+      .click();
+    browser.assert.urlEquals(this.pixelpainterurl_base);
+  });
+
+  it('Painter button', (browser) => {
+    browser
+      .element.findByText('Painter')
+      .assert.visible();
+    browser
+      .element.findByText('Painter')
+      .click();
+    browser.assert.urlEquals(this.pixelpainterurl_base + "new");
+  });
+
+  it('Gallery button', (browser) => {
+    browser
+      .element.findByText('Gallery')
+      .assert.visible();
+    browser
+      .element.findByText('Gallery')
+      .click();
+    browser.assert.urlEquals(this.pixelpainterurl_base + "gallery");
+  });
+
+  // it('Light mode button', (browser) => {
+  //   browser
+  //     .element.
+  //     .assert.visible();
+  //   browser
+  //     .element.findByText('Gallery')
+  //     .click();
+  //   browser.assert.urlEquals(this.pixelpainterurl_base + "gallery");
+  // });
+});
