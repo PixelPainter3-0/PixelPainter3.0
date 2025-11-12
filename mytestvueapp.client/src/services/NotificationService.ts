ad16ff98b237594cebd7f85606692458826422e4
Zach Scharbarth <scharbarthz2133@my.uwstout.edu>
2025-11-12 11:06:04 -0600

    Notification control zach (#43)
    * added initial notification settings, you can enable and disable all notification types
    
    * fixed issues with github

--- mytestvueapp.client/src/services/NotificationService.ts
+++ mytestvueapp.client/src/services/NotificationService.ts
@@ -2,26 +2,28 @@ import Notification from "../entities/Notification";
 
 export default class NotificationService {
   public static async getNotifications(artistId: number): Promise<Notification[]> {
-    const allNotifications: Notification[] = [];
-    try {
-      const response = await fetch(
-        `/notification/GetNotifications?userId=${artistId}`
-      );
-      if(!response.ok){
-        throw new Error("Problem getting notifications");
-      }
-      const data = await response.json();
-      
-      for (const newNotification of data) {
-        allNotifications.push(newNotification as Notification);
-      }
+  const allNotifications: Notification[] = [];
+  try {
+    const response = await fetch(
+      `/notification/GetNotificationsForArtist?artistId=${artistId}`
+    );
 
-      return allNotifications;
-    } catch (error) {
-      console.error(error);
-      return allNotifications;
+    if (!response.ok) {
+      throw new Error("Problem getting notifications");
     }
+
+    const data = await response.json();
+
+    for (const newNotification of data) {
+      allNotifications.push(newNotification as Notification);
+    }
+
+    return allNotifications;
+  } catch (error) {
+    console.error("Error getting notifications:", error);
+    return allNotifications;
   }
+}
   public static async markCommentViewed(commentId: number): Promise<boolean> {
     try {
       const response = await fetch(`/notification/MarkCommentViewed`, {
@@ -55,4 +57,18 @@ export default class NotificationService {
       return false;
     }
   }
+    public static async updateNotificationsEnabled(artistId: number, notificationsEnabled: number): Promise<boolean> {
+      try {
+        const response = await fetch(`/notification/UpdateNotificationsEnabled`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ artistId, notificationsEnabled }),
+        });
+        return response.ok;
+      } catch (error) {
+        console.error("Error updating notificationsEnabled:", error);
+        return false;
+      }
+    }
+  
 }
